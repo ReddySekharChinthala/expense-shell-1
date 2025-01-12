0596f8f80559a63b0e5b3b8e07b604c998081aa1
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-01-12 12:19:46 +0530

    script-call

--- backend.sh
+++ backend.sh
@@ -42,7 +42,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
 #here if we check in putty shell for pwd the below path will come and after 2nd path from etc/ copied from doc
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
