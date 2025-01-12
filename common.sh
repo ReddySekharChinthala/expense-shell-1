dc1d550c5679f07ef36a2e97092f22b2fc0adc11
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-01-12 13:12:19 +0530

    script-call

--- common.sh
+++ common.sh
@@ -6,7 +6,7 @@ handle_error(){
     echo "Error occured at line number: $1, error command: $2"
 }
 
-trap trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
