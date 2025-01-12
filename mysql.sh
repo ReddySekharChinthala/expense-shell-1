ed75b77cbe719af568641be5627bd04bb9b448e5
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-01-12 13:08:15 +0530

    script-call

--- mysql.sh
+++ mysql.sh
@@ -25,7 +25,7 @@ mysql -h db.rsdevops78s.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$LO
 if [ $? -ne 0 ]
 then 
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL_Root_Password_SetUP"
+    VALIDATE $? "MyyySQL_Root_Password_SetUP"
 else
     echo -e "mysql root setup password is alread set..$G SKIPPING $N" 
 fi
